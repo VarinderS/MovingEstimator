ca7f645ecb23c0c0f99742045e72caee5138d78b
Varinder Singh <highpolyboy@hotmail.com>
2013-05-19 15:52:28 +1200

    support for search filter and sort order

--- MovingEstimator/Content/App/App.js
+++ MovingEstimator/Content/App/App.js
@@ -1,41 +1,44 @@
-﻿var ballPark = angular.module("ballPark", []).config(function ($routeProvider) {
-    $routeProvider.when("/", {
-        templateUrl: "/Content/App/views/prices.html",
-        controller: pricesController
-    });
+﻿var ballPark = angular.module("ballPark", ["ngResource", "ngSanitize"]).config(function ($routeProvider) {
+	$routeProvider.when("/", {
+		templateUrl: "/Content/App/views/prices.html",
+		controller: pricesController
+	});
+}).filter("highlight", function() {
+	return function(text, query, caseSensitive) {
+		text = text.toString();
+		if (query || angular.isNumber(query)) {
+			if (caseSensitive) {
+				text = text.split(query).join("<mark>" + query + "</mark>");
+			} else {
+				text = text.replace(new RegExp(query, 'gi'), '<mark>$&</mark>');
+			}
+			//return "found some query"
+		}
+		return text;
+	}
 });
 
-pricesController = function ($scope) {
-    $scope.prices = [
-        {
-            ID: 1,
-            FromLocationID: "1",
-            FromLocation: "Whangerei",
-            ToLocationID: 2,
-            ToLocation: "Auckland",
-            OneBdrm: "100",
-            ThreeBdrm: "300",
-            FiveBdrm: "500"
-        },
-        {
-            ID: 2,
-            FromLocationID: "3",
-            FromLocation: "Snells beach",
-            ToLocationID: 4,
-            ToLocation: "Te Puke",
-            OneBdrm: "500",
-            ThreeBdrm: "600",
-            FiveBdrm: "700"
-        },
-        {
-            ID: 3,
-            FromLocationID: "5",
-            FromLocation: "Rotorua",
-            ToLocationID: 6,
-            ToLocation: "Warkworth",
-            OneBdrm: "800",
-            ThreeBdrm: "300",
-            FiveBdrm: "100"
-        }
-    ];
+ballPark.factory("server", function($resource) {
+	return $resource('/api/prices.json/:id', { id: '@id' }, { update: { method: 'PUT' } });
+});
+
+pricesController = function ($scope, $filter, server) {
+	$scope.prices = server.query();
+
+	$scope.reverse = false;
+	$scope.predicate = "ID";
+	//$scope.sortCol = "FromLocation";
+
+	$scope.sortBy = function(field) {
+		if ($scope.predicate == field) {
+			$scope.reverse = !$scope.reverse;
+		} else {
+			$scope.predicate = field;
+		}
+	}
+
+	$scope.display = function(content) {
+		var highlight = $filter("highlight");
+		return highlight(content, $scope.search);
+	}
 }
